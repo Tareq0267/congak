a756708637366a61f45ef850eb3801871c952b38
Tareq0267 <u2001228@siswa.um.edu.my>
2025-12-29 21:54:52 +0800

    iconssss

--- sw.js
+++ sw.js
@@ -1,12 +1,26 @@
-const CACHE_NAME = "congak-v1";
+const CACHE_NAME = "congak-v2"; // bump version when you change files
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
   "./main.js",
   "./manifest.json",
+
+  // images
+  "./assets/logo.png",
+  "./assets/logo_background.png",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png",
+
+  // sounds
+  "./sounds/correct.mp3",
+  "./sounds/wrong.mp3",
+  "./sounds/end.mp3",
+
+  // echarts CDN (ok, but see note below)
   "https://cdn.jsdelivr.net/npm/echarts@5/dist/echarts.min.js"
 ];
+
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
